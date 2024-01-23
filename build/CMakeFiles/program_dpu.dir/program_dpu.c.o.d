c620f0dbba83a270577f58ac9ec29ae5ff2aaf53
Mpoki Mwaisela <50764757+MpokiAbel@users.noreply.github.com>
2024-01-23 15:56:18 +0100

    Delete build directory

--- build/CMakeFiles/program_dpu.dir/program_dpu.c.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/program_dpu.dir/program_dpu.c.o: \
-  /home/pim/MPOKI/pim/program_dpu.c \
-  /usr/bin/../share/upmem/include/syslib/mram.h \
-  /usr/bin/../share/upmem/include/stdlib/stdint.h \
-  /usr/bin/../share/upmem/include/stdlib/limits.h \
-  /usr/bin/../share/upmem/include/syslib/attributes.h \
-  /usr/bin/../share/upmem/include/stdlib/stdio.h \
-  /usr/bin/../share/upmem/include/stdlib/stddef.h
